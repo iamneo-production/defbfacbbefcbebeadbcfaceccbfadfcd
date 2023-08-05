f395ba7fc9286bf4de648efec9e503d878144963
Iamneo <techadmin@iamneo.ai>
2023-08-05 05:35:48 +0000

    Backup Commit

--- angularapp/src/app/currencyconv.pipe.ts
+++ angularapp/src/app/currencyconv.pipe.ts
@@ -1,62 +1,39 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+interface ExchangeRate {
+  id: string;
+  value: number;
+}
+
+const exchangeRates: ExchangeRate[] = [
+  { id: 'USD', value: 1.126735 },
+  { id: 'GBP', value: 0.876893 },
+  { id: 'INR', value: 79.677056 },
+];
+
 @Pipe({
-  name: 'currencyconv'
+  name: 'currencyConverter'
 })
-export class CurrencyconvPipe implements PipeTransform {
+export class CurrencyConverterPipe implements PipeTransform {
+  transform(
+    amount: number,
+    fromCurrency: string,
+    toCurrency: string
+  ): number | string {
+    const sourceRate = this.getExchangeRate(fromCurrency);
+    const targetRate = this.getExchangeRate(toCurrency);
 
-  transform(value: number, from:String,to:String): any {
-
-    if(from == to)
-    {
-      return value;
+    if (sourceRate && targetRate) {
+      // Convert the amount from the source currency to the target currency
+      const convertedAmount = (amount / sourceRate) * targetRate;
+      return convertedAmount.toFixed(2);
     }
 
-    if(from == '0.876893')
-    {
-      from = 'GBP';
-    }
-    else if (from == '1.126735') 
-    {
-      from = 'USD';
-    } 
-    else {
-      from = 'INR';
-    }
-
-    if (to == '0.876893') 
-    {
-      to = 'GBP';
-    } 
-    else if (to == '1.126735') 
-    {
-      to = 'USD';
-    } 
-    else 
-    {
-      to = 'INR';
-    }
-
-    interface Exchange {
-      id :String;
-      value :number;
-    }
-
-    const exchangeRates: Exchange[] = 
-    [
-      {id: 'USD',  value: 1.126735 },
-      {id: 'GBP',  value: 0.876893 },
-      {id: 'INR',  value: 79.677056}
-    ];
-
-    const fromvalue: any = exchangeRates.find((exchange) => exchange.id == from)!.value;
-
-    const tovalue: any = exchangeRates.find((exchange) => exchange.id == to)!.value;
-
-
-    let result: number = Math.round((value * tovalue) / fromvalue);
-    //result = parseInt(result.toFixed(2));
-    return result.toFixed(2);
+    return 'Invalid currency code';
   }
 
+  private getExchangeRate(currencyCode: string): number | null {
+    const exchangeRate = exchangeRates.find(rate => rate.id === currencyCode);
+    return exchangeRate ? exchangeRate.value : null;
+  }
 }
