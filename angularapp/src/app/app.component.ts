453b7213649747b68bf934fbc3ddcd6b08755dd3
Iamneo <techadmin@iamneo.ai>
2023-08-05 07:58:37 +0000

    Backup Commit

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -1,31 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'pipes';
-  fromCurr :String = '';
-  toCurr   :string = '';
-  amount !:number;
-  result: string = 'sample';
-  show:boolean = false;
-
-
-  showResult()
-  {
-    console.log(this.fromCurr);
-    console.log(this.toCurr);
-    console.log(this.amount);
-    if(this.fromCurr !='' && this.toCurr != '')
-    {
-      this.show = true;
-    }
-    else
-    {
-      this.show = false;
-    }
+export class AppComponent implements OnInit {
+  title = 'converter';
+  fromCurrency: string="";
+  toCurrency: string="";
+  amount ="";
+  resValue: string="";
+  tocurr:string ="";
+  fromcurr:string ="";
+  value: string="";
+  selectfrom(event:any){
+    this.fromCurrency = event.target.value;
+    this.fromcurr=this.fromCurrency;
   }
+  selectto(event:any){
+    this.toCurrency=event.target.value;
+    this.tocurr=this.toCurrency;
+  }
+  getVal(val: string){
+    let total = Math.round(Number(val)*Number(this.tocurr)/Number(this.fromcurr));
+    var value = total.toFixed(2);
+    this.resValue = value;
+  }
+  constructor(){}
+  ngOnInit(){
+    
+  }
+
 }
